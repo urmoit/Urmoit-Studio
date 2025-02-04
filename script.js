aa27388a00da3fab514d5062120a1678637549ef
urmoit <89414990+urmoit@users.noreply.github.com>
2025-02-04 18:13:14 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-    const links = document.querySelectorAll(".animated-link");
-    links.forEach(link => {
-        const rect = link.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-            link.classList.add("visible");
-        }
-    });
-});
